5ed50a454d534c0fc610b680c6b752611c427664
Luisda1828 <LUIS.VILLEGASHERRERA@ucr.ac.cr>
2025-02-15 11:44:57 -0600

    feat: implement routing in App component with React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
-
+import { Link, Route, Navigate, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 const App = () => {
     // Estado para controlar si el Sidebar está abierto o cerrado
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -12,7 +13,7 @@ const App = () => {
     };
 
     return (
-        <div className="flex flex-col h-screen">
+      <div className="flex flex-col h-screen">
             {/* Navbar */}
             <Navbar toggleSidebar={toggleSidebar} />
 
@@ -25,10 +26,16 @@ const App = () => {
                 <div
                     className={`flex-1 p-4 overflow-y-auto transition-all duration-200 ${isSidebarOpen ? 'ml-[17%] ' : 'ml-0'
                         }`}
-                >
-                    <h1 className="text-xl font-bold">Contenido Principal</h1>
-                    {/* Puedes agregar más contenido aquí */}
+                        >
+                 
+                  <Routes>
+                    <Route path="/" element={<Home title={"Home"} />} />
+                    <Route path="/plaqueados" element={<Home title={"Plaquedos"} />} />
+                    <Route path="/no-plaqueados" element={<Home title={"No Plaquedos"} />} />
+                  </Routes> 
+       
                 </div>
+                {/* Termina el contenido principal */}
             </div>
         </div>
     );
