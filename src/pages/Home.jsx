4e6680dd97c4eabf07a5652dd2deabac88f93928
Luisda1828 <LUIS.VILLEGASHERRERA@ucr.ac.cr>
2025-02-15 11:45:07 -0600

    feat: add Home component with title prop

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = ({title}) => {
+    return (
+        <div className="flex flex-col">
+        <h1 className="text-xl font-bold">{title} </h1>
+        </div>
+    );
+    }
+    export default Home;
