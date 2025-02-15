5b2b774aa689b80690baeb6f87f74fb08a92e5bb
Luisda1828 <LUIS.VILLEGASHERRERA@ucr.ac.cr>
2025-02-15 11:44:44 -0600

    feat: replace anchor with React Router Link in SidebarItem component

--- src/components/SidebarItem.jsx
+++ src/components/SidebarItem.jsx
@@ -1,7 +1,11 @@
-const SidebarItem = ({ title, href = "#" }) => (
-    <a href={href} className="block p-1.5 px-3 rounded transition duration-200 hover:bg-gray-700">
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SidebarItem = ({ title, href = "#"  }) => (
+    <Link className='block p-1.5 px-3 rounded transition duration-200 hover:bg-gray-700' to={href}>
         {title}
-    </a>
+    </Link>
 );
 
 export default SidebarItem;
