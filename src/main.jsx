1bac0b49e8cb5fa49b0b957bfa00f1201f81702a
Luisda1828 <LUIS.VILLEGASHERRERA@ucr.ac.cr>
2025-02-15 10:41:35 -0600

    fix: remove redundant import of index.css in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.jsx'
