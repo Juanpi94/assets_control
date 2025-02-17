eff2cd74e96350f5f597c1a8d8f0b31d5f4560fb
Luisda1828 <LUIS.VILLEGASHERRERA@ucr.ac.cr>
2025-02-16 23:06:25 -0600

    feat: add Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Button = ({ text, onClick, Styles }) => {
+    return (
+        <button
+        className={Styles}
+        onClick={onClick}
+        >
+        {text}
+        </button>
+    );
+    }
+
+    export default Button;
