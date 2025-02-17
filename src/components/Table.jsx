eb976cd788c220f1f604a6574f1eabda09216e7a
Luisda1828 <LUIS.VILLEGASHERRERA@ucr.ac.cr>
2025-02-16 23:06:43 -0600

    feat: enhance Table component with custom styles

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,12 +3,50 @@ import DataTable from 'react-data-table-component';
 const Table = ({ data }) => {
   if (!data || data.length === 0) return <p>No hay datos disponibles</p>;
 
-  // Generar columnas a partir de los datos
   const columns = Object.keys(data[0]).map(key => ({
     name: key,
     selector: row => row[key],
     sortable: true,
   }));
+
+  const customStyles = {
+    table: {
+      style: {
+        boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', 
+        borderRadius: '5px',
+        border: '1px solid #e0e0e0',
+      },
+    },
+    rows: {
+      style: {
+        minHeight: '72px',
+        fontSize: '16px',
+      },
+      stripedStyle: {
+        backgroundColor: '#f1f1f1', 
+        color: 'black',
+      },
+    },
+    headCells: {
+      style: {
+        paddingLeft: '8px', 
+        paddingRight: '8px',
+      },
+    },
+    cells: {
+      style: {
+        paddingLeft: '8px', 
+        paddingRight: '8px',
+        fontSize: '16px',
+      }, 
+    },
+    head: {
+      style: {
+        fontSize: '18px',
+        fontWeight: 'bold',
+      },
+    },
+  };
 
   return (
     <DataTable
@@ -16,6 +54,10 @@ const Table = ({ data }) => {
       data={data}
       pagination
       selectableRows
+      striped  
+      customStyles={customStyles}
+      paginationPerPage={8}  // Establece 5 filas por página
+      paginationRowsPerPageOptions={[5, 8, 10, 15, 20]}  // Establece las opciones de filas por página
     />
   );
 };
