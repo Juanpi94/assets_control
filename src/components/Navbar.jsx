4d037c1ef9d17e06700ead93fdf2f2c378d93b8d
Juanpi94 <juang.94.ucr@gmail.com>
2025-02-18 16:24:40 -0600

    feat: update index.html title and favicon; remove unused SVG files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,26 +5,18 @@ const Navbar = ({ toggleSidebar }) => {
     return (
         <nav className="bg-blue-600 p-4 fixed w-full top-0 z-50">
             <div className="flex items-center justify-between">
-                <button
-                    onClick={toggleSidebar}
-                    className="text-white focus:outline-none"
-                >
-                    {/* Icono de menú */}
-                    <svg
-                        className="w-6 h-6"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M4 6h16M4 12h16m-7 6h7"
-                        ></path>
-                    </svg>
-                </button>
+                <div className='flex'>
+                    <button onClick={toggleSidebar} className="text-white focus:outline-none">
+                        {/* Icono de menú */}
+                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7">
+                            </path>
+                        </svg>
+                    </button>
+                    <div className="ml-3">
+                        <a className='text-white font-bold p-2' href="/">Control de activos</a>
+                    </div>
+                </div>
 
                 {/* Componente ProfileMenu */}
                 <ProfileMenu />
