951446f7c86e4f4b7a992a61528b6d0459c17ef7
Juanpi94 <juang.94.ucr@gmail.com>
2025-03-28 13:12:48 -0600

    fix: update axiosClient baseURL to localhost for local development

--- src/hooks/axiosClients.js
+++ src/hooks/axiosClients.js
@@ -3,7 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 
 export const axiosClient = axios.create({
-  baseURL: `http://10.35.100.99:8082/api/`,
+  // baseURL: `http://10.35.100.99:8082/api/`, // Cambiar por la dirección del servidor
+  baseURL: `http://127.0.0.1:8000/api`, // localhost
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
